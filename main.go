2e56806e108e335924c50ac9f8764d23645e3c0d
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-03-11 23:49:38 +0100

    new commit

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"Systray-Icon/systrayicon/systrayicon"
 	"fmt"
+
+	"github.com/rhomari/systrayicon"
 )
 
 func main() {
--- systrayicon/winGUI.h
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-#include <stdbool.h>
-
-HWND CreateForm();
-bool CreateTrayIcon();
-bool DestroyTrayIcon();
-void OnMouseClickEvent(int);
-extern void GoOnMouseClickEvent(int);
